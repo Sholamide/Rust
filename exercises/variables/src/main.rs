6b8933528264e8b04eb200ad595b4256645c2ef5
‘lordieXXL’ <caramorton40@gmail.com>
2024-01-10 11:51:44 +0100

    variable exercises 1-4

---
+++ exercises/variables/src/main.rs
@@ -0,0 +1,57 @@
+// variables1.rs
+//
+// Make me compile!
+//
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+// fn main() {
+//     let x:u32 = 5;
+//     println!("x has the value {x}!");
+// }
+
+// variables2.rs
+//
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+// fn main() {
+//     let x;
+//     if x == 10 {
+//         println!("x is ten!");
+//     } else {
+//         println!("x is not ten!");
+//     }
+// }
+
+// variables3.rs
+//
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+// fn main() {
+//     let x: i32 = 50;
+//     println!("Number {}", x);
+// }
+
+// variables4.rs
+//
+// Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+// fn main() {
+//     let mut x = 3;
+//     println!("Number {}", x);
+//     x = 5; // don't change this line
+//     println!("Number {}", x);
+// }
+
+
---
+++ exercises/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/Olamide.Sholuade/Desktop/rust/exercises/variables/target/debug/variables: /Users/Olamide.Sholuade/Desktop/rust/exercises/variables/src/main.rs
