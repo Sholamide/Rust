d30d0283767bc748222539915ba8439fd25bc381
‘lordieXXL’ <caramorton40@gmail.com>
2024-01-10 11:57:15 +0100

    variables 1-4 exercises

---
+++ ownership/target/debug/deps/ownership-39723e55aed0c312.d
@@ -0,0 +1,5 @@
+/Users/Olamide.Sholuade/Desktop/rust/ownership/target/debug/deps/libownership-39723e55aed0c312.rmeta: src/main.rs
+
+/Users/Olamide.Sholuade/Desktop/rust/ownership/target/debug/deps/ownership-39723e55aed0c312.d: src/main.rs
+
+src/main.rs:
